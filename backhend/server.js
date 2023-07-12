c7b70a7a56e392d99376fcb844c99519686209e0
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-07-12 09:14:37 +0530

    updated the docker file

---
+++ backhend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+var app = express();
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT;
+var hostname = "127.0.0.1";
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const connectDb = require("./config/db");
+connectDb();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
+
+//api call
+app.use("/api/build", require("./routes/buildRoutes"));
+
+
+app.listen(PORT, () => {
+  console.log(`listening on ${hostname}:${PORT}`);
+});
+
